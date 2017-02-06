46bc2a5fe41a251ca228b15a479d10463d42a794
Alexey Golub <tyrrrrrr@gmail.com>
2017-02-06 20:52:11 +0200

    Revert automatic constructor logic

--- Settings/CustomContractResolver.cs
+++ Settings/CustomContractResolver.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
@@ -10,21 +9,6 @@ namespace Tyrrrz.Settings
     internal sealed class CustomContractResolver : DefaultContractResolver
     {
         public static CustomContractResolver Instance { get; } = new CustomContractResolver();
-
-        private static bool IsCustomStruct(Type objectType)
-        {
-            return
-                objectType.IsValueType && !objectType.IsPrimitive && !objectType.IsEnum &&
-                !string.IsNullOrWhiteSpace(objectType.Namespace) && !objectType.Namespace.StartsWith("System.");
-        }
-
-        private static ObjectConstructor<object> CreateParameterizedConstructor(MethodBase method)
-        {
-            var c = method as ConstructorInfo;
-            if (c != null)
-                return a => c.Invoke(a);
-            return a => method.Invoke(null, a);
-        }
 
         private static bool IsIgnored(Type declaringType, string propertyName)
         {
@@ -36,29 +20,9 @@ namespace Tyrrrz.Settings
         protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
         {
             return base.CreateProperties(type, memberSerialization)
-                    // Not ignored
-                    .Where(p => IsIgnored(type, p.UnderlyingName))
-                    // Writable and readable
-                    .Where(p => p.Writable && p.Readable)
-                    .ToList();
-        }
-
-        protected override JsonObjectContract CreateObjectContract(Type objectType)
-        {
-            var contract = base.CreateObjectContract(objectType);
-            if (!IsCustomStruct(objectType)) return contract;
-
-            // For custom-defined constructs, find the most specific constructor instead
-            var constructors = objectType.GetConstructors(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-            var mostSpecific = constructors.OrderByDescending(c => c.GetParameters().Length).FirstOrDefault();
-            if (mostSpecific != null)
-            {
-                contract.OverrideCreator = CreateParameterizedConstructor(mostSpecific);
-                contract.CreatorParameters.Clear();
-                foreach (var param in CreateConstructorParameters(mostSpecific, contract.Properties))
-                    contract.CreatorParameters.Add(param);
-            }
-            return contract;
+                // Not ignored
+                .Where(p => !IsIgnored(type, p.UnderlyingName))
+                .ToList();
         }
     }
 }
